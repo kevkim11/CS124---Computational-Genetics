ad643c03a0854357b8448b4afc022686ecc71671
kevin_kim <kevkim1120@gmail.com>
2016-04-01 14:51:52 -0700

    dfd

--- Rosalind/Strings and Lists.py
+++ Rosalind/Strings and Lists.py
@@ -1,6 +1,6 @@
 def string_slicer(s, a, b, c, d):
     """
-    :param a, b: A string s of length at most 200 letters and four integers a, b, c and d.
+    :param s, a, b, c, d: A string s of length at most 200 letters and four integers a, b, c and d.
     :return: The slice of this string from indices a through b and c through d (with space in between), inclusively.
     """
     return s[a:b+1]+" "+s[c:d+1]
